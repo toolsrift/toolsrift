a7c2806466f45d3905df352887a99f92d3a562f3
toolsrift <toolsrift@gmail.com>
2026-04-15 18:14:49 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,21 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
-export default function Document() {
+export default function Document(props) {
+  const host = props.__NEXT_DATA__?.props?.pageProps?.__host || ''
+
+  let manifest = '/manifest.json'
+  if (host.startsWith('text.'))  manifest = '/manifest-text.json'
+  if (host.startsWith('image.')) manifest = '/manifest-image.json'
+  if (host.startsWith('pdf.'))   manifest = '/manifest-pdf.json'
+  if (host.startsWith('dev.'))   manifest = '/manifest-dev.json'
+  if (host.startsWith('calc.'))  manifest = '/manifest-calc.json'
+
   return (
     <Html lang="en">
       <Head>
-        <link rel="manifest" href="/manifest.json" />
-<meta name="theme-color" content="#38BDF8" />
-<link rel="apple-touch-icon" href="/icon-192.png" />
+        <link rel="manifest" href={manifest} />
+        <meta name="theme-color" content="#38BDF8" />
+        <link rel="apple-touch-icon" href="/icon-192.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
