64966640d4e92f0187d34fc4192feb8886773828
toolsrift <toolsrift@gmail.com>
2026-04-15 18:16:29 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,7 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Document from 'next/document'
 
-export default function Document(props) {
-  const host = props.__NEXT_DATA__?.props?.pageProps?.__host || ''
-
-  let manifest = '/manifest.json'
-  if (host.startsWith('text.'))  manifest = '/manifest-text.json'
-  if (host.startsWith('image.')) manifest = '/manifest-image.json'
-  if (host.startsWith('pdf.'))   manifest = '/manifest-pdf.json'
-  if (host.startsWith('dev.'))   manifest = '/manifest-dev.json'
-  if (host.startsWith('calc.'))  manifest = '/manifest-calc.json'
-
+export default function MyDocument({ manifest }) {
   return (
     <Html lang="en">
       <Head>
@@ -29,4 +21,18 @@ export default function Document(props) {
       </body>
     </Html>
   )
+}
+
+MyDocument.getInitialProps = async (ctx) => {
+  const initialProps = await Document.getInitialProps(ctx)
+  const host = ctx.req?.headers?.host || ''
+
+  let manifest = '/manifest.json'
+  if (host.startsWith('text.'))  manifest = '/manifest-text.json'
+  if (host.startsWith('image.')) manifest = '/manifest-image.json'
+  if (host.startsWith('pdf.'))   manifest = '/manifest-pdf.json'
+  if (host.startsWith('dev.'))   manifest = '/manifest-dev.json'
+  if (host.startsWith('calc.'))  manifest = '/manifest-calc.json'
+
+  return { ...initialProps, manifest }
 }
